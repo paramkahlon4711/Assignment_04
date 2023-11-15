bc989db86f936426aa594e3b1941a701942c2346
Param Kahlon <psinghkahlon@rrc.ca>
2023-11-15 13:08:43 -0600

    updated readme file

--- README.md
+++ README.md
@@ -8,11 +8,70 @@ in this assignment i will be exception proofing the pixell transactions bank app
 Param 
 
 ## Assignment
-the changes i made in the code are listed below:
-1. this piece of code produces an exception whenever the invalid input is inputted in the code: 
- if transaction_type not in valid_transaction_types:
+1. adding the Transaction Type Validation and Transaction Amount Validation to incorporate the data validation to add transaction is valid or not :
+if transaction_type not in valid_transaction_types:
                 valid_record = False
                 error_message += 'Invalid transaction type. '
 
-2. 
+ try:
+                # Extract the transaction amount from the third column
+                transaction_amount = float(row[2])
+            except ValueError:
+                valid_record = False
+                error_message += 'Non-numeric transaction amount. '
+2. Collecting  Invalid Records to procide differently if any records identified as invalid :
+rejected_records.append((row, error_message))
+## code modification
+preparing for Troubleshooting by copying bank_data and the deleting all files except first entry and then running:
+try:
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    with open('bank_data.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+to:
+print("\nREJECTED RECORDS\n================")
+
+## code modification 
+Updating code by run and Debug code, updating and adding some command to troubleshoot in which correcting the formula and value correction is included, with including  rejected records and error message in code:
+ry:
+                # Extract the transaction amount from the third column
+                transaction_amount = float(row[2])
+            except ValueError:
+                valid_record = False
+                error_message += 'Non-numeric transaction amount. '
+
+            if valid_record:
+                # updating  the transaction_counter for valid records
+                transaction_count += 1
+                total_transaction_amount += transaction_amount
+
+                if customer_id not in customer_data:
+                    customer_data[customer_id] = {'balance': 0, 'transactions': []}
+
+                if transaction_type == 'deposit':
+                    customer_data[customer_id]['balance'] += transaction_amount
+                elif transaction_type == 'withdraw':
+                    customer_data[customer_id]['balance'] -= transaction_amount
+and
+ adding rejected records and error massage
+    for record, error in rejected_records:
+        print("REJECTED RECORD:", record)
+        print("ERROR MESSAGE:", error)
+testing the data and comparing the code for each transaction value and applying the debug:
+
+    print("PiXELL River Transaction Report\n===============================\n")
+    for customer_id, data in customer_data.items():
+        balance = data['balance']
+        print(f"\nCustomer {customer_id} has a balance of {balance}.")
+        print("Transaction History:")
+        for transaction in data['transactions']:
+            amount, type = transaction
+            print(f"\t{type.capitalize()}: {amount}")
+
+    if transaction_count > 0:
+        print(f"\nAVERAGE TRANSACTION AMOUNT: ${(total_transaction_amount / transaction_count):,.2f}")
+    else:
+        print("\nNo valid transactions found.")
+
+    print("\nREJECTED RECORDS\n================")
 
--- pixell_transaction_report.py
+++ pixell_transaction_report.py
@@ -1,53 +1,51 @@
 """
 Description: A program that reads through transaction records and reports the results.
 Author: ACE Faculty
-Edited by: Param
-Date: 2023-11-15
+Edited by: yash chaudhari
+Date: 20/10/2023
 Usage: This program will read transaction data from a .csv file, summarize and 
 report the results.
 """
 import csv
 import os
- 
+
 valid_transaction_types = ['deposit', 'withdraw']
 customer_data = {}
 rejected_records = []
 transaction_count = 0
-transaction_counter = 0
 total_transaction_amount = 0
-valid_record = True
-error_message = ''
 
-os.system('cls' if os.name == 'nt' else 'clear')
+try:
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    with open('bank_data.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        ## adding the first row header
+        next(reader)
+        for row in reader:
+            # Reset valid record and error message for each iteration
+            valid_record = True
+            error_message = ''
 
-with open('bank_data.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        # Reset valid record and error message for each iteration
-        valid_record = True
-        error_message = ''
-
-        # Extract the customer ID from the first column
-        customer_id = row[0]
-        
-        # Extract the transaction type from the second column
-        transaction_type = row[1]
-        ### VALIDATION 1 ###
-        if transaction_type not in valid_transaction_types:
+            # Extract the customer ID from the first column
+            customer_id  = row[0] 
+           
+            # Extract the transaction type from the second column
+            transaction_type = row[1] 
+            ## adding invalid transation type
+            if transaction_type not in valid_transaction_types:
                 valid_record = False
                 error_message += 'Invalid transaction type. '
-        ## adding try-block for transcation_amount
-        try:
+
+            ## adding try-block for transcation_amount
+            try:
                 # Extract the transaction amount from the third column
                 transaction_amount = float(row[2])
-        except ValueError:
+            except ValueError:
                 valid_record = False
-                error_message += 'Non-numeric transaction amount.'
+                error_message += 'Non-numeric transaction amount. '
 
-        # Extract the transaction amount from the third column
-        ### VALIDATION 2 ###
-        if valid_record:
+            if valid_record:
                 # updating  the transaction_counter for valid records
                 transaction_count += 1
                 total_transaction_amount += transaction_amount
@@ -61,47 +59,32 @@ with open('bank_data.csv', 'r') as csv_file:
                     customer_data[customer_id]['balance'] -= transaction_amount
 
                 customer_data[customer_id]['transactions'].append((transaction_amount, transaction_type))
-        else:
+            else:
                 # Record invalid records
                 rejected_records.append((row, error_message))
-        transaction_amount = float(row[2])
 
-        if valid_record:
-            # Initialize the customer's account balance if it doesn't already exist
-            if customer_id not in customer_data:
-                customer_data[customer_id] = {'balance': 0, 'transactions': []}
+    
+    print("PiXELL River Transaction Report\n===============================\n")
+    for customer_id, data in customer_data.items():
+        balance = data['balance']
+        print(f"\nCustomer {customer_id} has a balance of {balance}.")
+        print("Transaction History:")
+        for transaction in data['transactions']:
+            amount, type = transaction
+            print(f"\t{type.capitalize()}: {amount}")
 
-            # Update the customer's account balance based on the transaction type
-            elif transaction_type == 'deposit':
-                customer_data[customer_id]['balance'] += transaction_amount
-                transaction_count += 1
-                total_transaction_amount += transaction_amount
-            elif transaction_type == 'withdrawal':
-                customer_data[customer_id]['balance'] += transaction_amount
-                transaction_count += 1
-                total_transaction_amount += transaction_amount
-            
-            # Record  transactions in the customer's transaction history
-            customer_data[customer_id]['transactions'].append((transaction_amount, transaction_type))
-        
-        ### COLLECT INVALID RECORDS ###
-        
+    if transaction_count > 0:
+        print(f"\nAVERAGE TRANSACTION AMOUNT: ${(total_transaction_amount / transaction_count):,.2f}")
+    else:
+        print("\nNo valid transactions found.")
 
+    print("\nREJECTED RECORDS\n================")
+    
+    for record, error in rejected_records:
+        print("REJECTED RECORD:", record)
+        print("ERROR MESSAGE:", error)
 
-print("PiXELL River Transaction Report\n===============================\n")
-# Print the final account balances for each customer
-for customer_id, data in customer_data.items():
-    balance = data['balance']
-
-    print(f"\nCustomer {customer_id} has a balance of {balance}.")
-    # Print the transaction history for the customer
-    print("Transaction History:")
-    for transaction in data['transactions']:
-        amount, type = transaction
-        print(f"\t{type.capitalize()}: {amount}")
-
-print(f"\nAVERAGE TRANSACTION AMOUNT: {(total_transaction_amount / transaction_counter)}")
-
-print("\nREJECTED RECORDS\n================")
-for record in rejected_records:
-    print("REJECTED:", record)
+except FileNotFoundError as e:
+    print(f"ERROR: {e} ")
+except Exception as e:
+    print(f"ERROR: {e} ")
